9b56839870dfed3720b7c7a9cf11e1b1c876913e
Gigi Sayfan <gigi.sayfan@helix.com>
2017-04-20 11:55:32 -0700

    Bump version to 0.2 to accomodate for README.md change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pathology',
-      version='0.1',
+      version='0.2',
       url='https://github.com/the-gigi/pathology',
       license='MIT',
       author='Gigi Sayfan',
